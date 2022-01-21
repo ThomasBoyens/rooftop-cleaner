7c9c63d8fb806f7649980e9323cbb883994d5ec7
bartbit <bartpijkels@hotmail.com>
2022-01-21 15:03:46 +0100

    scene4
    werkende UI + pickup scripts.

---
+++ rooftop-cleaner-game/Assets/Scripts/SolarPanelInvent.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SolarPanelInvent : MonoBehaviour
+{
+
+    public static SolarPanelInvent instance;
+    public Text ScoreText;
+    private int score = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = score.ToString() + "/25";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        ScoreText.text = score.ToString() + "/25";
+    }
+}
